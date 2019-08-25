0b5eeba9f216198784b0ca35c0216842ccd7cfc8
Mike Vessia <mike@vessia.net>
2019-08-25 10:10:12 -0400

    Add support links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import UnitConverter from "./components/UnitConverter";
+import styled from "styled-components";
+
+const Footer = styled.footer`
+  bottom: 0;
+  height: 5%;
+  text-align: center;
+`;
 
 function App() {
   return (
     <div className="App">
       <UnitConverter />
+      <Footer>
+        Please send any problems or requests to this <a href="mailto:mike+kilocalc@vessia.net">email</a> or <a href="https://instagram.com/lambdalift">instagram</a>
+      </Footer>
     </div>
   );
 }
