3be28570a1292f465fee6aff7abc952d085ab7a8
Mike Vessia <mike@vessia.net>
2019-08-25 11:54:07 -0400

    Breaking inputs down into separate components

--- src/components/UnitConverter.js
+++ src/components/UnitConverter.js
@@ -2,74 +2,110 @@ import React, { Fragment, useState } from "react";
 import { Form, FormGroup, ButtonGroup, Button, Input, Container, Row, Col } from "reactstrap";
 import BarbellsView from "./BarbellsView";
 
-
-const WeightInput = () => {
-
-  const [weight, setWeight] = useState(0);
-  const [unit, setUnit] = useState("kg");
-  const [rounding, setRounding] = useState("nearest");
+const Inputs = () => {
 
   const defaultBarAndCollarWeight = unit => {
     return unit === "kg" ? 25 : 45;
   };
 
+  const [weight, setWeight] = useState(0);
+  const [unit, setUnit] = useState("kg");
+  const [rounding, setRounding] = useState("nearest");
   const [barAndCollarWeight, setBarAndCollarWeight] = useState(defaultBarAndCollarWeight(unit));
+
+  return (
+    <Fragment>
+      <Form>
+        <WeightInput
+          unit={unit}
+          setUnit={setUnit}
+          rounding={rounding}
+          setRounding={setRounding}
+          weight={weight}
+          setWeight={setWeight}
+          barAndCollarWeight={barAndCollarWeight}
+          setBarAndCollarWeight={setBarAndCollarWeight}
+        />
+        <Row>
+          <Col>
+            <UnitInput
+              unit={unit}
+              setUnit={setUnit}
+              setBarAndCollarWeight={setBarAndCollarWeight}
+              defaultBarAndCollarWeight={defaultBarAndCollarWeight}
+            />
+          </Col>
+          <Col>
+            <RoundingInput
+              rounding={rounding}
+              setRounding={setRounding}
+            />
+          </Col>
+        </Row>
+      </Form>
+
+      <hr/>
+
+      <BarbellsView
+        weight={weight}
+        barAndCollarWeight={barAndCollarWeight}
+        unit={unit}
+        rounding={rounding}
+      />
+    </Fragment>
+  );
+};
+
+const RoundingInput = ({ rounding, setRounding }) => {
+
+  return (
+    <Fragment>
+      <h4>Rounding</h4>
+      <ButtonGroup>
+        <Button color="primary" onClick={() => setRounding("nearest")} active={rounding === "nearest"}>nearest</Button>
+        <Button color="primary" onClick={() => setRounding("up")} active={rounding === "up"}>up</Button>
+        <Button color="primary" onClick={() => setRounding("down")} active={rounding === "down"}>down</Button>
+      </ButtonGroup>
+    </Fragment>
+  );
+};
+
+const UnitInput = ({ unit, setUnit, setBarAndCollarWeight, defaultBarAndCollarWeight }) => {
 
   const updateUnit = unit => {
     setUnit(unit);
     setBarAndCollarWeight(defaultBarAndCollarWeight(unit));
   };
 
+  return (
+    <Col>
+      <h4>Unit</h4>
+      <ButtonGroup>
+        <Button color="primary" onClick={() => updateUnit("kg")} active={unit === "kg"}>kg</Button>
+        <Button color="primary" onClick={() => updateUnit("lbs")} active={unit === "lbs"}>lbs</Button>
+      </ButtonGroup>
+    </Col>
+  );
+};
+const WeightInput = ({ unit, weight, setWeight, barAndCollarWeight, setBarAndCollarWeight }) => {
+
 
   return (
-    <Fragment>
-      <FormGroup>
-        <Container>
-          <h4>Total Weight</h4>
-          <Input step=".01" id="weightInput" type="number" onChange={e => setWeight(e.target.value)} />
+    <FormGroup>
+      <Container>
+        <h4>Total Weight</h4>
+        <Input step=".01" id="weightInput" type="number" onChange={e => setWeight(e.target.value)} />
 
-          <h4>Bar And Collar Weight</h4>
-          <Input step=".01" id="barAndCollar" type="number" value={barAndCollarWeight} onChange={e => setBarAndCollarWeight(e.target.value)} />
-
-
-          <Row>
-            <Col>
-              <h4>Unit</h4>
-              <ButtonGroup>
-                <Button color="primary" onClick={() => updateUnit("kg")} active={unit === "kg"}>kg</Button>
-                <Button color="primary" onClick={() => updateUnit("lbs")} active={unit === "lbs"}>lbs</Button>
-              </ButtonGroup>
-            </Col>
-
-            <Col>
-              <h4>Rounding</h4>
-              <ButtonGroup>
-                <Button color="primary" onClick={() => setRounding("nearest")} active={rounding === "nearest"}>nearest</Button>
-                <Button color="primary" onClick={() => setRounding("up")} active={rounding === "up"}>up</Button>
-                <Button color="primary" onClick={() => setRounding("down")} active={rounding === "down"}>down</Button>
-              </ButtonGroup>
-            </Col>
-          </Row>
-        </Container>
-      </FormGroup>
-
-      <div>
-        <BarbellsView
-          weight={weight}
-          barAndCollarWeight={barAndCollarWeight}
-          unit={unit}
-          rounding={rounding}
-        />
-      </div>
-    </Fragment>
+        <h4>Bar And Collar Weight</h4>
+        <Input step=".01" id="barAndCollar" type="number" value={barAndCollarWeight} onChange={e => setBarAndCollarWeight(e.target.value)} />
+      </Container>
+    </FormGroup>
   );
 };
 
 const UnitConverter = () => {
   return (
-    <Form>
-      <WeightInput />
-    </Form>
+    <Inputs />
   );
 };
 
